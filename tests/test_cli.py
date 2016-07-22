4039098e68aa11322d05ffa57932788836505c1e
Mathew Le <matle@ucdavis.edu>
2016-07-22 11:27:50 -0700

    Add test for 'mv' move

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,11 +19,8 @@ def runner():
   return CliRunner()
 
 def wait_restart():
-  i = 0
   while not client.ping():  
     time.sleep(0.5)
-    print 'sleeping for %s half seconds' % i
-    i = i + 1
   '''
   for i in range(0,25):
     if client.ping():
@@ -307,6 +304,101 @@ def test_mv_rename(runner):
   shutil.rmtree(target_dir, ignore_errors=True)
 
   assert True
+
+def test_mv_move(runner):
+  """
+    1. 'kdr add' a path to the json
+    2. move it into another directories
+    3. check if move worked
+    4. if worked, revert and 'kdr free', else False
+  """
+
+  source_dir = os.path.join(client.adapter.home_dir, 'Downloads', 'child123123')
+  target_dir = os.path.join(client.adapter.home_dir, 'Downloads', 'parent123123')
+  final_dir = os.path.join(client.adapter.home_dir, 'Downloads', 'parent123123', 'child123123')
+
+  if not os.path.exists(source_dir):
+    os.makedirs(source_dir)
+
+    if not os.path.exists(source_dir):
+      print "Failed to create %s" % source_dir
+      assert False
+
+  if not os.path.exists(target_dir):
+    os.makedirs(target_dir)
+
+    if not os.path.exists(target_dir):
+      print "Failed to create %s" % target_dir
+      assert False
+
+  result = runner.invoke(cli.add, [source_dir])
+
+  if result.exception:
+    print result.exception
+    assert False
+
+  wait_restart()
+  result = runner.invoke(cli.mv, [source_dir, target_dir])
+
+  if result.exception:
+    print result.exception
+    assert False
+
+  wait_restart()
+
+  if os.path.exists(source_dir):
+    print source_dir
+    print 'source_dir was not deleted'
+    assert False
+
+  if not os.path.exists(final_dir):
+    print "Failed to move %s to %s, %s doesn't exist" % (source_dir, target_dir, final_dir)
+    assert False
+
+  syncthing_config = client.get_config()
+  kdr_config = client.adapter.get_config()
+  folders = syncthing_config['folders']
+  old_key = client.adapter.get_dir_id(source_dir)
+  new_key = client.adapter.get_dir_id(final_dir)
+  found = False
+
+  for f in folders:
+    if final_dir == os.path.abspath(f['path']):
+
+      if new_key != f['id']:
+        print 'New folder id has failed to be added'
+        assert False
+
+      if f['id'] == old_key:
+        print 'Old folder id is still there'
+        assert False
+
+      found = True
+      break
+
+  if not found:
+    print 'New path hasn\'t been added to syncthing_config'
+    assert False
+
+  if new_key not in kdr_config['directories'] or old_key in kdr_config['directories']:
+    print 'Modifying kdr_config has failed'
+    assert False
+
+  result = runner.invoke(cli.free, [final_dir])
+
+  if result.exception:
+    print result.exception
+    assert False
+
+  wait_restart()
+
+  shutil.rmtree(source_dir, ignore_errors=True)
+  shutil.rmtree(target_dir, ignore_errors=True)
+  shutil.rmtree(final_dir, ignore_errors=True)
+
+  assert True
+
+
 
 '''
 def test_cli(runner):
