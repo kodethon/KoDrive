850de935462d37a415e4dfe72e2416a169bc8a19
Michael Yen <lvlichael8@gmail.com>
2016-07-25 18:21:50 -0700

    Linux Path Fix

--- kdr/platform_adapter.py
+++ kdr/platform_adapter.py
@@ -256,13 +256,14 @@ class SyncthingLinux64(PlatformBase):
     # If syncthing doesn't exist, install it
     if not os.path.exists(syncthing_path):
       dest_tmp = '/tmp'
-      #linux_64_bit_repo = "https://github.com/syncthing/syncthing/releases/download/v%s" % self.st_version
 
+      #linux_64_bit_repo = "https://github.com/syncthing/syncthing/releases/download/v%s" % self.st_version
       linux_64_bit_tar = "syncthing-linux-amd64-v%s.tar.gz" % self.st_version
       linux_64_bit_repo = "http://cumulus.cs.ucdavis.edu/kdr/"
-
-      command = "wget -P %s %s" % (dest_tmp, os.path.join(linux_64_bit_repo, linux_64_bit_tar))
-      subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
+      
+      if not os.path.exists(dest_tmp, linux_64_bit_tar):
+        command = "wget -P %s %s" % (dest_tmp, os.path.join(linux_64_bit_repo, linux_64_bit_tar))
+        subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
       src = dest_tmp
       command = "tar -zxvf %s/%s --directory %s" % (src, linux_64_bit_tar, dest)
