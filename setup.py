b4e756c124c246b5f6d71a6973e649a6e382a5eb
Michael Yen <lvlichael8@gmail.com>
2016-07-29 14:05:56 -0700

    Added Global Installation

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependencies = ['click', 'requests']
 
 setup(
     name='kdr',
-    version='0.9.0',
+    version='0.9.1',
     url='https://github.com/Jvlythical/KodeDrive',
     license='LICENSE',
     author='Michael Yen',
