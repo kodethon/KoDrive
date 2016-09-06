e39e5c69743b8429080716f2137034c182f9475a
Matt Le <matle@ucdavis.edu>
2016-09-06 15:45:17 -0700

    Add Mac OSX bug fixes

--- tests/test_autostart.py
+++ tests/test_autostart.py
@@ -10,7 +10,8 @@ import shutil, subprocess, platform
 from mock import cli as mock
 
 
-# Only tests Mac OSX for now
+# NOTE: Only tests Mac OSX for now,
+# refactor when Linux autostart written
 
 @pytest.fixture
 def runner():
@@ -19,17 +20,6 @@ def runner():
 def test_autostart(runner):
   ''' Ensures that kodrive will autostart  '''
 
-  result = runner.invoke(cli.sys, ['start'])
-  mock.client.wait_start(0.5, 10)
-
-  if result.exception:
-    print result.exception
-    assert False
-
-  if not mock.client.ping():
-    assert False
-
-  kodrive_config = mock.client.adapter.get_config()
   system = platform.system()
 
   if system == "Linux":
@@ -37,30 +27,34 @@ def test_autostart(runner):
     assert True
 
   elif system == "Darwin":
+    result = runner.invoke(cli.sys, ['start'])
+    mock.client.wait_start(0.5, 10)
+
+    if result.exception:
+      print result.exception
+      assert False
+
+    if not mock.client.ping():
+      assert False
+
+    kodrive_config = mock.client.adapter.get_config()
     plist_name = 'com.kodrive.autostart'
 
     daemon_list = subprocess.check_output([
       'launchctl', 'list'   
-    ])
+    ], stderr=subprocess.STDOUT)
 
     if plist_name not in daemon_list:
+      print daemon_list
+      print '--------------------end of daemon list---------------------'
       print 'kodrive was not added to the autostart list'
       assert False
 
-  result = runner.invoke(cli.sys, ['stop'])
+    result = runner.invoke(cli.sys, ['stop'])
 
-  if result.exception:
-    print result.exception
-    assert False
-
-  if system == "Linux":
-    print 'Linux does not autostart yet.'
-    assert True
-
-    # if not mock.client.ping():
-    #   assert False
-
-  elif system == "Darwin":
+    if result.exception:
+      print result.exception
+      assert False
 
     if mock.client.ping():
       result = runner.invoke(cli.sys, ['stop'])
