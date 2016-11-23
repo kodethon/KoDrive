4b9a01d9401565e76a993b5d5bc003481519c871
Michael Yen <lvlichael@Gmail.Com>
2016-11-23 14:07:39 -0800

    Syncthing Config Tweaks

--- kodrive/data/syncthing_adt.py
+++ kodrive/data/syncthing_adt.py
@@ -5,7 +5,7 @@ class Folder:
     if 'rescanIntervalS' in kwargs:
       rescanIntervalS = kwargs['rescanIntervalS']   
     else:
-      rescanIntervalS = 5
+      rescanIntervalS = 15
 
     self.folder = {
       'rescanIntervalS' : rescanIntervalS,
